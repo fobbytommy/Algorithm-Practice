67e6c169188a749e992501b13e058829af0e6e4c
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-08-22 16:55:10 -0700

    second_to_last has been updated to consider for array with fewer than 2 values.

--- 10_week_2/second_to_last.js
+++ 10_week_2/second_to_last.js
@@ -1,8 +1,13 @@
 // Given an array, return the second-to-last element
 
 function second_to_last(arr) {
+	if (arr.length < 2) {
+		return null;
+	}
 	return arr[arr.length-2];
 }
 
 var arr = [1, 2, "this should be returned", 4];
+// var arr = ["should return null"];
+
 console.log(second_to_last(arr));
--- 10_week_2/second_to_last.py
+++ 10_week_2/second_to_last.py
@@ -1,7 +1,10 @@
 # Given an array, return the second-to-last element.
 
 def second_to_last(arr):
+	if len(arr) < 2:
+		return None
 	return arr[len(arr)-2]
 
 arr = [1, 2, "this should be returned", 4]
+# arr = ['should return None']
 print(second_to_last(arr))
