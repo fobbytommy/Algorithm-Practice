f7215221f02819f71bbfae19f2c0626a9afe103f
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-11-02 21:57:09 -0700

    updated insertion_sort.js

--- 11_sorts/02_insertion_sort/insertion_sort.js
+++ 11_sorts/02_insertion_sort/insertion_sort.js
@@ -2,15 +2,13 @@ var arr  = [3, 5, 2, 4, 6, 1];
 
 function insertion_sort(arr) {
 	for (var i = 1; i < arr.length; i++) {
+		var element = arr[i];
 		var j = i;
-
-		while (j > 0 && arr[j] < arr[j - 1]) {
-			var temp = arr[j];
+		while (j > 0 && arr[j - 1] > element) {
 			arr[j] = arr[j - 1];
-			arr[j - 1] = temp;
-
 			j--;
 		}
+		arr[j] = element;
 	}
 }
 
